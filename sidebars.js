61a051bc30fa836615a9d3e3d11aa35938661ebb
Christopher Little <clittle@gsma.com>
2020-06-22 09:52:14 +0100

    Use explicit syntax for sidebars

--- sidebars.js
+++ sidebars.js
@@ -1,35 +1,37 @@
 module.exports = {
-  someSidebar: {
-    Introduction: [
-      'intro',
-      'docsguide'
-    ],
-    Architecture: [
-      "architecture/overview",
-      "architecture/connections",
-      "architecture/matching",
-    ],
-    Installation: [
-      "installation/introduction",
-      "installation/itp",
-      "installation/mmo",
-      "installation/sp",
-      "installation/mojaloop",
-    ],
-    "Use Cases": [
-      "usecases", 
-      "uclist", 
-      "uccreation"
-    ],
-    "Test Cases": [
-      "tclist", 
-      "tctemplate"
-    ],
-    "User Manual": [
-      "quickguide", 
-      "session", 
-      "tests", 
-      "results"
-    ],
-  },
+  docs: [
+    "intro",
+    "docsguide",
+    {
+      type: "category",
+      label: "Architecture",
+      items: [
+        "architecture/overview",
+        "architecture/connections",
+        "architecture/matching",
+      ],
+    },
+    {
+      type: "category",
+      label: "Installation",
+      items: [
+        "installation/introduction",
+        "installation/itp",
+        "installation/mmo",
+        "installation/sp",
+        "installation/mojaloop",
+      ],
+    },
+    {
+      type: "category",
+      label: "Use Cases",
+      items: ["usecases", "uclist", "uccreation"],
+    },
+    { type: "category", label: "Test Cases", items: ["tclist", "tctemplate"] },
+    {
+      type: "category",
+      label: "User Manual",
+      items: ["quickguide", "session", "tests", "results"],
+    },
+  ],
 };
